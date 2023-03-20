a0ce96ec04c34306b1853c333af8d388a17c14c1
hgparkk <phg2559@naver.com>
2023-03-20 21:36:53 +0900

    new commit

--- 알고리즘_1장/searchK/searchK.cpp
+++ 알고리즘_1장/searchK/searchK.cpp
@@ -1,7 +1,7 @@
 ﻿#include <stdio.h>
 #include <stdlib.h>
 
-int searchK(int K);
+int searchK(int NumArray[], int count, int K);
 void testSearchK();
 
 int main()
@@ -9,33 +9,37 @@ int main()
 	testSearchK();
 }
 
-int searchK(int K)
+int searchK(int NumArray[],int count, int K)
 {
-	int A[10] = { 5,7,3,0,80,95,42,31,23,15 };
-	int ret = -1;
+	//1. result 에 초기 값 -1을 저장 한다
+	int result = -1;
 
-	for (int i = 0; i < 10; i++)
+	//2. 첫 번째 숫자를 읽어 찾고자 하는 숫자 K와 비교하여 같으면 result를 그 숫자로 갱신한다.
+	//3. 남은 숫자에 대해 2를 반복한다.
+	for (int i = 0; i < count; i++)
 	{
-		if (A[i] == K)
+		if (NumArray[i] == K)
 		{
-			ret = K;
+			result = K;
 			break;
 		}
 	}
 
-	return ret;
+	//4. 찾고자 하는 수 result를 반환한다.
+	return result;
 }
 
 void testSearchK()
 {
-	int fval = 15;
-	int xret = -2;
+	int A[] = { 45, 20, 60, 35, 10, 55, 90, 85, 75, 25 };
+	int fvalue = 85;
+	int xresult = -1;
 
-	xret = searchK(fval);
-	if (xret == fval) {
-		printf("%d 값을 찾았습니다. %d\n", fval, xret);
+	xresult = searchK(A, sizeof(A) / sizeof(int),fvalue);
+	if (xresult == fvalue) {
+		printf("%d 값을 찾았습니다. %d\n", fvalue, xresult);
 	}
 	else {
-		printf("%d 값을 찾지 못하였습니다. %d\n", fval, xret);
+		printf("%d 값을 찾지 못하였습니다. %d\n", fvalue, xresult);
 	}
 }
