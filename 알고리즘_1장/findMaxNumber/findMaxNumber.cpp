0c51c0e89ea9110cd92342d6c6891afdae96c3fd
hgparkk <phg2559@naver.com>
2023-03-20 20:23:12 +0900

    first commit

---
+++ 알고리즘_1장/findMaxNumber/findMaxNumber.cpp
@@ -0,0 +1,35 @@
+﻿#include <stdio.h>
+
+int findMaxNumber(int NumArray[], int count)
+{
+	int max = -1;
+
+	//1. 처음 숫자를 읽어 최대값 변수 max 에 저장한다.
+	max = NumArray[0];
+
+	//2. 다음 숫자를 읽어 max와 비교하여 더 큰 숫자면 max를 갱신한다.
+	//3. 남은 숫자에 대해 2를 반복한다.
+	for (int i = 1; i < count; i++)
+	{
+		if (NumArray[i] > max) 
+			max = NumArray[i];
+	}
+
+	//4. 가장 큰 수 max를 반환한다.
+	return max;
+}
+
+int test_findMaxNumber()
+{
+	int Array[] = { 45,20,35,60,55,10,90,85,75,25 };
+	int maxNum = -1;
+
+	maxNum = findMaxNumber(Array, sizeof(Array) / sizeof(int));
+	printf("최댓값은 %d 입니다.\n", maxNum);
+	return 0;
+}
+
+int main(void)
+{
+	test_findMaxNumber();
+}
