cd855f3fe55b3f5b8f53dc5ebf9bd17f4dd12ff5
hgparkk <127173002+hgparkk@users.noreply.github.com>
2023-03-20 12:10:56 +0900

    Add files via upload

---
+++ 알고리즘_2장/EuclidUseRecursion.cpp
@@ -0,0 +1,17 @@
+﻿
+#include <stdio.h>
+
+int Euclid(int a, int b)
+{
+	if (b == 0)
+		return a;
+	else
+		return Euclid(b, a % b);
+}
+
+int main()
+{
+	int a = Euclid(24, 14);
+	printf("%d\n", a);
+	return 0;
+}
---
+++ 알고리즘_2장/EuclidUseWhile.cpp
@@ -0,0 +1,23 @@
+﻿
+#include <stdio.h>
+
+int Euclid(int a, int b) 
+{
+	if (a < b)
+		return 0;
+	int basv;
+	while (b != 0) 
+	{
+		basv = b;
+		b = a % b;
+		a = basv;
+	}
+	return a;
+}
+
+int main()
+{
+	int a = Euclid(24, 14);
+	printf("%d\n", a);
+	return 0;
+}
