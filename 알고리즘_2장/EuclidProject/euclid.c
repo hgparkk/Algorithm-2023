866ebb6b61c79cf1a5b416ab3020da2b02bb1c8f
hgparkk <phg2559@naver.com>
2023-04-04 16:59:40 +0900

    home commit

---
+++ 알고리즘_2장/EuclidProject/euclid.c
@@ -0,0 +1,24 @@
+﻿
+#include <stdio.h>
+
+int euclidRepeatitive(int a, int b)
+{
+	if (a < b)
+		return 0;
+	int basv;
+	while (b != 0)
+	{
+		basv = b;
+		b = a % b;
+		a = basv;
+	}
+	return a;
+}
+
+int euclidRecursive(int a, int b)
+{
+	if (b == 0)
+		return a;
+	else
+		return euclidRecursive(b, a % b);
+}
---
+++ 알고리즘_2장/EuclidProject/test.c
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+#include "euclid.h"
+
+
+int testEuclid()
+{
+	int a = euclidRecursive(48, 34);
+	printf("%d 와 %d의 최대공약수는 %d 입니다.\n", 48, 34, a);
+	return 0;
+}
