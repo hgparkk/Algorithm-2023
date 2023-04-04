d73b734eab35aca94b71bf26688d23a21fd20dab
hgparkk <127173002+hgparkk@users.noreply.github.com>
2023-04-04 14:59:52 +0900

    Add files via upload

---
+++ 알고리즘_2장/EuclidProject/euclid.h
@@ -0,0 +1,5 @@
+#pragma once
+
+//Function Prototypes
+int euclidRepeatitive(int a, int b);
+int euclidRecursive(int a, int b);
---
+++ 알고리즘_2장/EuclidProject/main.cpp
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+#include "test.h"
+
+
+int main()
+{
+    int ret = 0;
+
+    ret = testEuclid();
+}
