b606c4874a83b3b1ffa8ad0db0d5f7b31d509714
현규 박 <phg2559@naver.com>
2023-04-05 15:02:41 +0900

    new commit

--- 알고리즘_2장/EuclidProject/test.cpp
+++ 알고리즘_2장/EuclidProject/test.cpp
@@ -25,18 +25,36 @@ int testEuclid()
 		}
 	}
 
+	long long a1array[1000];
 	clock_t startTime1 = clock();
 	for (int i = 0; i < 1000; i++)
 	{
 		long long a1 = euclidRecursive(a[i], b[i]);
+		a1array[i] = a1;
 		printf("%lld 와 %lld의 최대공약수는 %lld 입니다. \n", a[i], b[i], a1);
-		long long a2 = euclidRepeatitive(a[i], b[i]);
-		printf("%lld 와 %lld의 최대공약수는 %lld 입니다. \n", a[i], b[i], a2);
 	}
 	clock_t finishTime1 = clock();
+
+	long long a2array[1000];
+	clock_t startTime2 = clock();
+	for(int i = 0; i < 1000; i++)
+	{
+		long long a2 = euclidRepeatitive(a[i], b[i]);
+		a2array[i] = a2;
+		printf("%lld 와 %lld의 최대공약수는 %lld 입니다. \n", a[i], b[i], a2);
+	}
+	clock_t finishTime2 = clock();
+
+	for (int i = 0; i < 1000; i++)
+	{
+		if (a1array[i] != a2array[i])
+			printf("계산 오류");
+	}
+
 	double timeSec1 = (double)((finishTime1 - startTime1) / CLOCKS_PER_SEC);
+	double timeSec2 = (double)((finishTime2 - startTime2) / CLOCKS_PER_SEC);
 	
-	printf(" 실행시간은 %lf 입니다 \n", timeSec1);
+	printf("두 함수의 실행시간은 각각 %lf초, %lf초 입니다 \n", timeSec1, timeSec2);
 
 	return 0;
 }
