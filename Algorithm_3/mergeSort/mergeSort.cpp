08900e7dc6179a4ca4013b47bb0d8ccafd79fa18
현규 박 <phg2559@naver.com>
2023-04-10 12:40:56 +0900

    new commit

---
+++ Algorithm_3/mergeSort/mergeSort.cpp
@@ -0,0 +1,49 @@
+﻿
+#include <stdio.h>
+#define SIZE 8
+int tmp[SIZE];
+
+void merge(int A[], int p, int q) 
+{
+    int C[SIZE];
+    int ai, bi, ci;
+    int k = (p + q) / 2;
+    ai = p;
+    bi = k + 1;
+    ci = 0;
+    while (ci <= q - p + 1)
+    {
+        if (A[ai] < A[bi]) {
+            C[ci++] = A[ai++];
+        }
+        else {
+            C[ci++] = A[bi++];
+        }
+    }
+    ci = 0;
+    ai = p;
+    while (ai <= q - p + 1)
+        A[ai++] = C[ci++];
+
+}
+
+void mergeSort(int A[], int p, int q)
+{
+    if (p == q) return;
+    int k = (p + q) / 2;
+    mergeSort(A, p, k);
+    mergeSort(A, k + 1, q);
+    merge(A, p, q);
+}
+
+void printArray(int A[], int size) {
+    for (int i = 0; i < size; i++)
+        printf("%d ", A[i]);
+}
+
+int main() {
+    int A[SIZE] = { 37,10,22,30,35,13,25,24 };
+
+    mergeSort(A, 0, SIZE - 1);
+    printArray(A, SIZE);
+}
