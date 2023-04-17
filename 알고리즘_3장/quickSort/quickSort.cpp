56009df4405da62dd961ca6277716b03febe3b64
현규 박 <phg2559@naver.com>
2023-04-17 12:35:29 +0900

    new commit

---
+++ 알고리즘_3장/quickSort/quickSort.cpp
@@ -0,0 +1,21 @@
+﻿
+#include <stdio.h>
+#define SIZE 8
+
+void quickSort(int A[], int left, int right)
+{
+    int pivot;
+    int low = left;
+    int high = right + 1;
+    pivot = A[left];
+}
+
+void printArray(int A[], int size) {
+    for (int i = 0; i < size; i++)
+        printf("%d ", A[i]);
+}
+
+int main() {
+    int A[SIZE] = { 37,10,22,30,35,13,25,24 };
+    printArray(A, SIZE);
+}
