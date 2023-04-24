b9648edc15a816bcf51ac167dfea58fdc115c957
현규 박 <phg2559@naver.com>
2023-04-24 12:25:49 +0900

    new commmit

--- 알고리즘_3장/quickSort/quickSort.cpp
+++ 알고리즘_3장/quickSort/quickSort.cpp
@@ -1,12 +1,35 @@
 ﻿
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #define SIZE 8
 
 void quickSort(int A[], int left, int right)
 {
     int pivot;
     int low = left;
-    int high = right + 1;
+    int high = right;
+    long seed = time(NULL);
+    srand(seed);
+    pivot = rand() % (right - left);
+    int p = A[pivot];
+    int zero = A[left];
+    A[left] = p;
+    A[pivot] = zero;
+
+    for (int i = 1; i < right - left; i++)
+    {
+        int tmp = A[i];
+        if (tmp < p)
+        {
+
+            A[i] = tmp;
+        }
+        else if (tmp > p)
+        {
+            A[p + 1] = tmp;
+        }
+    }
 }
 
 void printArray(int A[], int size) {
